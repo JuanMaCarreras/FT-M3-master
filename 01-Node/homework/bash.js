b4f68cc497a8269e2b45a885e2ce94020f3c1987
JuanMaCarrearas <manuucarreras@gmail.com>
2022-11-15 14:14:31 -0300

    teminado

--- 01-Node/homework/bash.js
+++ 01-Node/homework/bash.js
@@ -1,7 +1,18 @@
-console.log(Object.keys(process))
+const commands = require("./commands");
 
 
+process.stdout.write(" $ ");
 
+function termine(info) {
+  process.stdout.write(info);
+  process.stdout.write("\n > ");
+}
 
+process.stdin.on("data", function (data) {
+  var cmd = data.toString().trim().split(" ");
+  let comando = cmd.shift();
 
-
+  if (commands[comando]) {
+    commands[comando](cmd, termine);
+  } else termine("El comando no existe");
+});
