5e4bbbb3daa654a2ea79414235dd91a3a56eced2
JuanMaCarrearas <manuucarreras@gmail.com>
2022-11-17 10:35:03 -0300

    terminado

--- 03-WebServer/homework/showByName/app.js
+++ 03-WebServer/homework/showByName/app.js
@@ -1,4 +1,21 @@
-var fs  = require("fs")
-var http  = require("http")
+const fs = require("fs")
+const http = require("http")
 
 // Escribí acá tu servidor
+http.createServer((req, res) => {
+    fs.readFile(`./images${req.url}.jpg`, (err, data) => {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/plain' })
+            res.end(' Image not found')
+        } else {
+
+            res.writeHead(200, { 'Content-Type': 'image/jpg' })
+            res.end(data)
+        }
+
+    })
+
+}).listen(1337, '127.0.0.1')
+
+
+
